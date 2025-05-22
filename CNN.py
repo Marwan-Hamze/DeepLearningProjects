2107298fff09dd9f417288700a49b2e2de717dfa
Marwan-Hamze <marwan2905@hotmail.com>
2025-05-22 15:34:24 +0900

    Added Save and Load model options

--- CNN.py
+++ CNN.py
@@ -9,6 +9,8 @@ from torch.utils.data import DataLoader
 from torchmetrics import Accuracy, Precision, F1Score, ConfusionMatrix
 import seaborn as sns
 from tqdm.auto import tqdm # progress bar
+import argparse
+from pathlib import Path
 
 '''
 This is a simple implementation of a Convolutional Neural Network (CNN) for a multi-class image classification.
@@ -62,104 +64,176 @@ class CNN(nn.Module):
         out = self.classifier(block2_out)
         return out
 
-# Setup training data
-train_data = datasets.FashionMNIST(
-    root="./Images", # where to download data to?
-    train=True, # get training data
-    download=False, # download data if it doesn't exist on disk
-    transform=ToTensor(), # images come as PIL format, we want to turn into Torch tensors
-    target_transform=None # you can transform labels as well
-)
+parser = argparse.ArgumentParser()
 
-# Setup testing data
-test_data = datasets.FashionMNIST(
-    root="./Images",
-    train=False, # get test data
-    download=False,
-    transform=ToTensor()
-)
-# Print informations about the datatset
-image, label = train_data[0] # Each element is a tuple of an image and its label
-# print("Shape of a single image:", image.shape)
-# How many samples are there? 
-# print(f"Number of Training Images: {len(train_data.data)}, Test Images: {len(test_data.data)}")
-# train_data.targets gives the number of targets, which is equal to the data since each image has a single target
-class_names  = train_data.classes # gives the classes(targets) names (a list of 10 elements)
+parser.add_argument("operation", type= str, choices=["train", "load"], help= "train and save the model, or load an existing one")
 
-# Visualize the image
-# plt.imshow(image.squeeze(), cmap= "gray") # image shape is [1, 28, 28] (colour channels, height, width)
-# plt.title(class_names[label])
-# plt.show()
+args, remaining_argv = parser.parse_known_args()
 
-# Setup the batch size hyperparameter
-BATCH_SIZE = 32
-
-# Turn datasets into iterables (batches)
-train_dataloader = DataLoader(train_data, # dataset to turn into iterable
-    batch_size=BATCH_SIZE, # how many samples per batch? 
-    shuffle=True # shuffle data every epoch?
-)
-
-test_dataloader = DataLoader(test_data,
-    batch_size=BATCH_SIZE,
-    shuffle=False # don't necessarily have to shuffle the testing data
-)
-
-# Let's check out what we've created
-# print(f"Length of train dataloader: {len(train_dataloader)} batches of {BATCH_SIZE}")
-# print(f"Length of test dataloader: {len(test_dataloader)} batches of {BATCH_SIZE}")
-
-
-# Initialize the model, loss, and optimizer
+# Assigning Variables used in both training and loading models
 
 hidden_layers = 10
+BATCH_SIZE = 32
 
-Model = CNN(1, hidden_layers, 10) # Input is the number of color channels, Output are the classes
-loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(params= Model.parameters(), lr= 0.1)
+if args.operation == "train":
 
-# Training the Model
-epochs = 3
-train_time_start_on_cpu = timer()
-for i in tqdm(range(epochs)):
-    print(f"Epoch: {i+1}\n-------")
-    train_loss_per_batch = 0
-    for batch, (X, y) in enumerate(train_dataloader):
-        Model.train()
-        output = Model(X)
-        loss = loss_fn(output, y)
-        train_loss_per_batch += loss
+    # Setup training data
+    train_data = datasets.FashionMNIST(
+        root="./Images", # where to download data to?
+        train=True, # get training data
+        download=False, # download data if it doesn't exist on disk
+        transform=ToTensor(), # images come as PIL format, we want to turn into Torch tensors
+        target_transform=None # you can transform labels as well
+    )
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+    # Setup testing data
+    test_data = datasets.FashionMNIST(
+        root="./Images",
+        train=False, # get test data
+        download=False,
+        transform=ToTensor()
+    )
+    # Print informations about the datatset
+    image, label = train_data[0] # Each element is a tuple of an image and its label
+    # print("Shape of a single image:", image.shape)
+    # How many samples are there? 
+    # print(f"Number of Training Images: {len(train_data.data)}, Test Images: {len(test_data.data)}")
+    # train_data.targets gives the number of targets, which is equal to the data since each image has a single target
+    class_names  = train_data.classes # gives the classes(targets) names (a list of 10 elements)
 
-        # Print out how many samples have been seen
-        if batch % 400 == 0:
-            print(f"Looked at {batch * len(X)}/{len(train_dataloader.dataset)} samples")
-    # Divide total train loss by length of train dataloader (average loss per batch per epoch)
-    train_loss_per_batch /= len(train_dataloader)
+    # Visualize the image
+    # plt.imshow(image.squeeze(), cmap= "gray") # image shape is [1, 28, 28] (colour channels, height, width)
+    # plt.title(class_names[label])
+    # plt.show()
 
-    ### Testing the Model along training
-    # Setup variables for accumulatively adding up loss
+    # Turn datasets into iterables (batches)
+    train_dataloader = DataLoader(train_data, # dataset to turn into iterable
+        batch_size=BATCH_SIZE, # how many samples per batch? 
+        shuffle=True # shuffle data every epoch?
+    )
+
+    test_dataloader = DataLoader(test_data,
+        batch_size=BATCH_SIZE,
+        shuffle=False # don't necessarily have to shuffle the testing data
+    )
+
+    # Let's check out what we've created
+    # print(f"Length of train dataloader: {len(train_dataloader)} batches of {BATCH_SIZE}")
+    # print(f"Length of test dataloader: {len(test_dataloader)} batches of {BATCH_SIZE}")
+
+    # Initialize the model, loss, and optimizer
+
+    Model = CNN(1, hidden_layers, 10) # Input is the number of color channels, Output are the classes
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(params= Model.parameters(), lr= 0.1)
+
+    # Training the Model
+    epochs = 5
+    train_time_start_on_cpu = timer()
+    for i in tqdm(range(epochs)):
+        print(f"Epoch: {i+1}\n-------")
+        train_loss_per_batch = 0
+        for batch, (X, y) in enumerate(train_dataloader):
+            Model.train()
+            output = Model(X)
+            loss = loss_fn(output, y)
+            train_loss_per_batch += loss
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            # Print out how many samples have been seen
+            if batch % 400 == 0:
+                print(f"Looked at {batch * len(X)}/{len(train_dataloader.dataset)} samples")
+        # Divide total train loss by length of train dataloader (average loss per batch per epoch)
+        train_loss_per_batch /= len(train_dataloader)
+
+        ### Testing the Model along training
+        # Setup variables for accumulatively adding up loss
+        test_loss = 0
+        Model.eval()
+        with torch.inference_mode():
+            for X, y in test_dataloader:
+                # 1. Forward pass
+                test_pred = Model(X)
+            
+                # 2. Calculate loss (accumulatively)
+                test_loss += loss_fn(test_pred, y) # accumulatively add up the loss per epoch
+            
+            # Calculations on test metrics need to happen inside torch.inference_mode()
+            # Divide total test loss by length of test dataloader (per batch)
+            test_loss /= len(test_dataloader)
+
+        ## Print out what's happening
+        print(f"\nTrain loss: {train_loss_per_batch:.5f} | Test loss: {test_loss:.5f}\n")
+
+    # Calculate training time      
+    train_time_end_on_cpu = timer()
+    total_train_time_model_0 = print_train_time(start=train_time_start_on_cpu, 
+                                            end=train_time_end_on_cpu)
+    # Saving the trained model
+
+    # Create models directory (if it doesn't already exist), see: https://docs.python.org/3/library/pathlib.html#pathlib.Path.mkdir
+    MODEL_PATH = Path("Computer_Vision_Models")
+    MODEL_PATH.mkdir(parents=True, # create parent directories if needed
+                    exist_ok=True # if models directory already exists, don't error
+        )
+
+    # Create model save path
+    MODEL_NAME = "Mnest_CNN.pth"
+    MODEL_SAVE_PATH = MODEL_PATH / MODEL_NAME
+    # Save the model state dict. For saving the model vs its state_dict, see Notes.txt
+
+    print(f"Saving model to: {MODEL_SAVE_PATH}")
+    torch.save(obj=Model.state_dict(), # only saving the state_dict() only saves the learned parameters
+            f=MODEL_SAVE_PATH)
+
+elif args.operation == "load":
+    parser.add_argument("--path", required=True, type=str)
+    args = parser.parse_args()
+
+    LOAD_PATH = args.path
+
+    print(LOAD_PATH)
+
+    # Create a new instance of FashionMNISTModelV2 (the same class as our saved state_dict())
+    # Note: loading model will error if the shapes here aren't the same as the saved version
+    loaded_model = CNN(1, hidden_layers, 10) 
+
+    # Load in the saved state_dict()
+    loaded_model.load_state_dict(torch.load(f=LOAD_PATH))
+    print(f"Model loaded: {LOAD_PATH}")
+
+    # Setting Testing Data
+    test_data = datasets.FashionMNIST(
+        root="./Images",
+        train=False, # get test data
+        download=False,
+        transform=ToTensor()
+    )
+
+    test_dataloader = DataLoader(test_data,
+        batch_size=BATCH_SIZE,
+        shuffle=False # don't necessarily have to shuffle the testing data
+    )
+
+    # Testing the Loaded Model
     test_loss = 0
-    Model.eval()
+    loss_fn = nn.CrossEntropyLoss()
+    
+    loaded_model.eval()
     with torch.inference_mode():
         for X, y in test_dataloader:
             # 1. Forward pass
-            test_pred = Model(X)
-           
+            test_pred = loaded_model(X)
+            
             # 2. Calculate loss (accumulatively)
             test_loss += loss_fn(test_pred, y) # accumulatively add up the loss per epoch
-        
-        # Calculations on test metrics need to happen inside torch.inference_mode()
-        # Divide total test loss by length of test dataloader (per batch)
-        test_loss /= len(test_dataloader)
+            
+            # Calculations on test metrics need to happen inside torch.inference_mode()
+            # Divide total test loss by length of test dataloader (per batch)
+            test_loss /= len(test_dataloader)
 
-    ## Print out what's happening
-    print(f"\nTrain loss: {train_loss_per_batch:.5f} | Test loss: {test_loss:.5f}\n")
-
-# Calculate training time      
-train_time_end_on_cpu = timer()
-total_train_time_model_0 = print_train_time(start=train_time_start_on_cpu, 
-                                           end=train_time_end_on_cpu)
+        ## Print out what's happening
+        print(f"\nTest loss: {test_loss:.5f}\n")
